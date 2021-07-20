fc1828f3d4ce6beeef6c9a234a1b2b87bc47f9bc
sujeong <tnwjd6496@naver.com>
2021-07-20 23:26:45 +0900

    [5주차] 사이드바 구현

---
+++ 5주차/PetWalk/PetWalk/ViewController/sideMenuViewController.swift
@@ -0,0 +1,29 @@
+//
+//  sideMenuViewController.swift
+//  PetWalk
+//
+//  Created by 김수정 on 2021/07/20.
+//
+
+import UIKit
+
+class sideMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
