bc660c5b9bca3e40fb0711e2a8c23faa68afd44c
sujeong <tnwjd6496@naver.com>
2021-07-22 17:05:25 +0900

    [5주차] 뷰컨트롤러 코드 정리

--- 5주차/PetWalk/PetWalk/Screen/ViewController/ViewController.swift
+++ 5주차/PetWalk/PetWalk/Screen/ViewController/ViewController.swift
@@ -15,13 +15,17 @@ import KakaoSDKUser
 
 class ViewController: UIViewController {
     
+    // MARK: - IBOulet
     @IBOutlet weak var kakaoLoginBtn: UIButton!
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
-    // 카카오소셜로그인(웹) → 시뮬레이터에
+    // MARK: - Kakao Social Login
+    // 카카오소셜로그인(웹) → 시뮬레이터에 카카오톡이 없으므로 웹으로 로그인 진행
+    // kakao login btn
     @IBAction func kakaoLoginBtnTouched(_ sender: Any) {
         
         UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
@@ -29,62 +33,37 @@ class ViewController: UIViewController {
                 print(error)
             }
             else {
+                // 로그인 성공
                 print("loginWithKakaoAccount() success.")
                 
-                //do something
+                // 액세스 토큰
                 _ = oauthToken
-                // 어세스토큰
                 let accessToken = oauthToken?.accessToken
                 
-                //카카오 로그인을 통해 사용자 토큰을 발급 받은 후 사용자 관리 API 호출
-                
+                // 로그인 성공시, 'UserInfoVC' 호출
                 guard let dvc = self.storyboard?.instantiateViewController(identifier: "UserInfoVC") as? UserInfoVC else {return}
                 
+                // 카카오 로그인을 통해 사용자 토큰을 발급 받은 후 사용자 관리 API 호출
                 UserApi.shared.me() {(user, error) in
                     if let error = error {
                         print(error)
                     }
                     else {
+                        // 사용자 정보 가져옴
                         print("me() success.")
-                        //do something
+                        
                         _ = user
+                        // 사용자의 닉네임을 받아 'nameLabel'에 넣어줌
                         dvc.nameLabel = user?.kakaoAccount?.profile?.nickname
                         
-                         userMainData.shared.loginUser = user?.kakaoAccount?.profile?.nickname
+                        // 사용자의 정보를 싱글톤으로 선언한 구조체에 넣어주어 어디든 접근 가능하게 함
+                        userMainData.shared.loginUser = user?.kakaoAccount?.profile?.nickname
                     }
                     
-                
+                    // 'UserInfoVC' 호출
                     self.navigationController?.pushViewController(dvc, animated: true)
-//                dvc.modalPresentationStyle = .fullScreen
-//                self.present(dvc, animated: true, completion: nil)
-                
-            
-                
-                
                 }
-                
             }
         }
     }
-    
-//    func setUserInfo() {
-//        UserApi.shared.me() {(user, error) in
-//            if let error = error {
-//                print(error)
-//            }
-//            else {
-//                print("me() success.")
-//                //do something
-//                _ = user
-//                self.infoLabel.text = user?.kakaoAccount?.profile?.nickname
-//            }
-//        }
-//    }
-
-    
-    @IBAction func kakaoLogoutBtnTouched(_ sender: Any) {
-   
-
-    }
-
 }
