4ca850a189049bd8dd9203cbcbc074585b380b0d
sujeong <tnwjd6496@naver.com>
2021-07-22 19:24:53 +0900

    [5주차] 코드정리

--- 5주차/PetWalk/PetWalk/Screen/ViewController/CustomSideMenuNavigation.swift
+++ 5주차/PetWalk/PetWalk/Screen/ViewController/CustomSideMenuNavigation.swift
@@ -13,14 +13,11 @@ class CustomSideMenuNavigation: SideMenuNavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 사이드 바 설정
         self.presentationStyle = .menuSlideIn
         self.leftSide = true
         
         //상단 상태바 보이도록 설정 0 ( 0~1 default 1 )
-              self.statusBarEndAlpha = 0.0
-        
+        self.statusBarEndAlpha = 0.0
     }
-    
-
-    
 }
--- 5주차/PetWalk/PetWalk/Screen/ViewController/tabBarVC.swift
+++ 5주차/PetWalk/PetWalk/Screen/ViewController/tabBarVC.swift
@@ -10,62 +10,52 @@ import FSCalendar
 
 class tabBarVC: UIViewController {
 
-    
+    // MARK: - Properties
     @IBOutlet weak var toggleTap: UIButton!
     @IBOutlet weak var calendarUIView: FSCalendar!
     
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //calendarUIView.select(Date())
-        
-        //setCalendarStyle()
-        
         setCalendarStyleWeek()
     }
     
-    
-
+    // MARK: - Function
+    // calendar week
     func setCalendarStyleWeek() {
         calendarUIView.scope = .week
         calendarUIView.accessibilityIdentifier = "calender"
         calendarUIView.appearance.headerMinimumDissolvedAlpha = 0.0
-        
         calendarUIView.locale = Locale(identifier: "ko_KR")
         calendarUIView.appearance.headerDateFormat = "M월"
         calendarUIView.headerHeight = 50
-
         calendarUIView.appearance.weekdayTextColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-
         calendarUIView.appearance.selectionColor = .none
         calendarUIView.appearance.titleSelectionColor = #colorLiteral(red: 0.02356945537, green: 0.7047216296, blue: 0.5695679188, alpha: 1)
         calendarUIView.appearance.todayColor = #colorLiteral(red: 0.02356945537, green: 0.7047216296, blue: 0.5695679188, alpha: 1)
         calendarUIView.appearance.todaySelectionColor = #colorLiteral(red: 0.02356945537, green: 0.7047216296, blue: 0.5695679188, alpha: 1)
         calendarUIView.appearance.headerTitleColor = .black
-      
     }
+    
+    // calendar month
     func setCalendarStyleMonth() {
         calendarUIView.scope = .month
         calendarUIView.accessibilityIdentifier = "calender"
         calendarUIView.appearance.headerMinimumDissolvedAlpha = 0.0
-        
         calendarUIView.locale = Locale(identifier: "ko_KR")
         calendarUIView.appearance.headerDateFormat = "M월"
         calendarUIView.headerHeight = 50
-
         calendarUIView.appearance.weekdayTextColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-
         calendarUIView.appearance.selectionColor = .none
         calendarUIView.appearance.titleSelectionColor = #colorLiteral(red: 0.02356945537, green: 0.7047216296, blue: 0.5695679188, alpha: 1)
         calendarUIView.appearance.todayColor = #colorLiteral(red: 0.02356945537, green: 0.7047216296, blue: 0.5695679188, alpha: 1)
         calendarUIView.appearance.todaySelectionColor = #colorLiteral(red: 0.02356945537, green: 0.7047216296, blue: 0.5695679188, alpha: 1)
         calendarUIView.appearance.headerTitleColor = .black
-      
     }
- 
+    
+    // MARK: - IBAction
     @IBAction func didTapToggle(_ sender: UIButton) {
         sender.isSelected = !sender.isSelected
-       
         if sender.isSelected == true {
             setCalendarStyleMonth()
         }
@@ -73,29 +63,4 @@ class tabBarVC: UIViewController {
             setCalendarStyleWeek()
         }
     }
-    
 }
-
-//extension tabBarVC: UICollectionViewDataSource {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return 2
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//
-//        if(indexPath.row == 0) {
-////            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: tabBarPageCVCell.identifier, for: indexPath) as? tabBarPageCVCell else{
-////                return UICollectionViewCell()
-////            }
-//
-//
-//            //return cell
-//        }
-//    return UICollectionViewCell()
-//    }
-//
-//}
-//
-//extension tabBarVC: UICollectionViewDelegate {
-//
-//}
