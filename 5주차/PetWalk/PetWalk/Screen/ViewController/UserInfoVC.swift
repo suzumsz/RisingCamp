ba34424ddb6fd76364e9552547816b833097b147
sujeong <tnwjd6496@naver.com>
2021-07-22 17:11:18 +0900

    [5주차] dogInfoVC 수정

--- 5주차/PetWalk/PetWalk/Screen/ViewController/UserInfoVC.swift
+++ 5주차/PetWalk/PetWalk/Screen/ViewController/UserInfoVC.swift
@@ -5,27 +5,27 @@
 //  Created by 김수정 on 2021/07/19.
 //
 
-
-
 import UIKit
 
 class UserInfoVC: UIViewController {
     
+    // MARK: - Properties
     var nameLabel : String?
-
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var dogNameTextField: UITextField!
     @IBOutlet weak var nextPageBtn: UIButton!
     @IBOutlet weak var cancelBtn: UIButton!
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         userNameLabel.text = nameLabel! + String("님!")
-
         textFieldStyle()
     }
     
-    
+    // MARK: - Function
+    // TextField Style
     func textFieldStyle() {
         let bottomLine = CALayer()
         bottomLine.frame = CGRect(x: 0.0, y: dogNameTextField.frame.height - 1, width: dogNameTextField.frame.width, height: 1.0)
@@ -33,26 +33,25 @@ class UserInfoVC: UIViewController {
         dogNameTextField.borderStyle = UITextField.BorderStyle.none
         dogNameTextField.layer.addSublayer(bottomLine)
     }
-
+    
+    // MARK: - IBAction
+    // 다음 페이지로 넘어갈 버튼
     @IBAction func didTapNextPage(_ sender: Any) {
         
+        // 다음 페이지 'DogInfoVC' 선언
         guard let dvc = self.storyboard?.instantiateViewController(identifier: "DogInfoVC") as? DogInfoVC else {return}
         
+        // 강아지 이름 싱글톤으로 선언한 구조체에 넣어주기
         dvc.dogName = dogNameTextField.text
         userMainData.shared.dogName = dvc.dogName
         
-//        dvc.modalPresentationStyle = .fullScreen
-//        self.present(dvc, animated: true, completion: nil)
+        // 'DogInfoVC' 호출
         self.navigationController?.pushViewController(dvc, animated: true)
-        
     }
     
- 
+    // 취소 버튼
     @IBAction func didTapCancelBtn(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
-        //self.dismiss(animated: true, completion: nil)
     }
-    
-
 }
 
